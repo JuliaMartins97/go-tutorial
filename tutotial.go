b74667754dd0cc3e1889a6deb73c941830863139
Julia Martins <martinss.s.julia@gmail.com>
2024-09-25 09:01:52 -0400

    Initial commit

---
+++ tutotial.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt" // used to output and collect user input
+
+// entry point
+func main() {
+	fmt.Println("Welcome to my quiz game!")
+
+	name := "Joe"
+	fmt.Println(name)
+}
+
+
